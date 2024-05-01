342f9e28a14739df83c30c831107d77c1144e620
feedop <filipskrzeczkowski@gmail.com>
2024-05-01 18:02:44 +0200

    Add project files.

---
+++ the-pink-puma/scene.cpp
@@ -0,0 +1,200 @@
+﻿#include "scene.h"
+#include <array>
+#include "mesh.h"
+
+using namespace mini;
+using namespace gk2;
+using namespace DirectX;
+using namespace std;
+
+RoomDemo::RoomDemo(HINSTANCE appInstance)
+	: DxApplication(appInstance, 1280, 720, L"Pokój"), 
+	//Constant Buffers
+	m_cbWorldMtx(m_device.CreateConstantBuffer<XMFLOAT4X4>()),
+	m_cbProjMtx(m_device.CreateConstantBuffer<XMFLOAT4X4>()),
+	m_cbViewMtx(m_device.CreateConstantBuffer<XMFLOAT4X4, 2>()),
+	m_cbSurfaceColor(m_device.CreateConstantBuffer<XMFLOAT4>()),
+	m_cbLightPos(m_device.CreateConstantBuffer<XMFLOAT4>()),
+
+	//Textures
+	// ?
+	
+	//Particles
+	m_particles{ {-1.3f, -0.6f, -0.14f} }
+{
+	//Projection matrix
+	auto s = m_window.getClientSize();
+	auto ar = static_cast<float>(s.cx) / s.cy;
+	XMStoreFloat4x4(&m_projMtx, XMMatrixPerspectiveFovLH(XM_PIDIV4, ar, 0.01f, 100.0f));
+	UpdateBuffer(m_cbProjMtx, m_projMtx);
+	UpdateCameraCB();
+
+	//Sampler States
+	SamplerDescription sd;
+
+	// Create sampler with appropriate border color and addressing (border) and filtering (bilinear) modes
+	sd.BorderColor[0] = 0;
+	sd.BorderColor[1] = 0;
+	sd.BorderColor[2] = 0;
+	sd.BorderColor[3] = 0;
+	sd.AddressU = sd.AddressV = sd.AddressW = D3D11_TEXTURE_ADDRESS_BORDER;
+	sd.Filter = D3D11_FILTER_MIN_MAG_LINEAR_MIP_POINT;
+	m_sampler = m_device.CreateSamplerState(sd);
+
+	//Meshes
+	vector<VertexPositionNormal> vertices;
+	vector<unsigned short> indices;
+	// TODO: Load robot
+	m_wall = Mesh::Rectangle(m_device, 4.0f);
+	m_cylinder = Mesh::Cylinder(m_device, 4, 9, 1.0f, 0.1f);
+
+	m_vbParticles = m_device.CreateVertexBuffer<ParticleVertex>(ParticleSystem::MAX_PARTICLES);
+
+	//World matrix of all objects
+	auto temp = XMMatrixTranslation(0.0f, 0.0f, 2.0f);
+	auto a = 0.f;
+	for (auto i = 0U; i < 4U; ++i, a += XM_PIDIV2)
+		XMStoreFloat4x4(&m_wallsMtx[i], temp * XMMatrixRotationY(a));
+	XMStoreFloat4x4(&m_wallsMtx[4], temp * XMMatrixRotationX(XM_PIDIV2));
+	XMStoreFloat4x4(&m_wallsMtx[5], temp * XMMatrixRotationX(-XM_PIDIV2));
+	
+	XMStoreFloat4x4(&m_cylinderMtx, XMMatrixRotationZ(XM_PIDIV2) * XMMatrixScaling(2.0f, 2.0f, 2.0f) * XMMatrixTranslation(0.0f, -2.0f, 1.0f));
+
+	// Light position
+	m_lightPos = { 0.0f, 0.9f, 0.0f, 1.0f };
+
+	//Render states
+	RasterizerDescription rsDesc;
+	rsDesc.CullMode = D3D11_CULL_NONE;
+	m_rsCullNone = m_device.CreateRasterizerState(rsDesc);
+
+	m_bsAlpha = m_device.CreateBlendState(BlendDescription::AlphaBlendDescription());
+	DepthStencilDescription dssDesc;
+	dssDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ZERO;
+	m_dssNoWrite = m_device.CreateDepthStencilState(dssDesc);
+
+	auto vsCode = m_device.LoadByteCode(L"phongVS.cso");
+	auto psCode = m_device.LoadByteCode(L"phongPS.cso");
+	m_phongVS = m_device.CreateVertexShader(vsCode);
+	m_phongPS = m_device.CreatePixelShader(psCode);
+	m_inputlayout = m_device.CreateInputLayout(VertexPositionNormal::Layout, vsCode);
+
+	vsCode = m_device.LoadByteCode(L"particleVS.cso");
+	psCode = m_device.LoadByteCode(L"particlePS.cso");
+	auto gsCode = m_device.LoadByteCode(L"particleGS.cso");
+	m_particleVS = m_device.CreateVertexShader(vsCode);
+	m_particlePS = m_device.CreatePixelShader(psCode);
+	m_particleGS = m_device.CreateGeometryShader(gsCode);
+	m_particleLayout = m_device.CreateInputLayout<ParticleVertex>(vsCode);
+
+	m_device.context()->IASetInputLayout(m_inputlayout.get());
+	m_device.context()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	//We have to make sure all shaders use constant buffers in the same slots!
+	//Not all slots will be use by each shader
+	ID3D11Buffer* vsb[] = { m_cbWorldMtx.get(),  m_cbViewMtx.get(), m_cbProjMtx.get() };
+	m_device.context()->VSSetConstantBuffers(0, 3, vsb); //Vertex Shaders - 0: worldMtx, 1: viewMtx,invViewMtx, 2: projMtx, 3: tex1Mtx, 4: tex2Mtx
+	m_device.context()->GSSetConstantBuffers(0, 1, vsb + 2); //Geometry Shaders - 0: projMtx
+	ID3D11Buffer* psb[] = { m_cbSurfaceColor.get(), m_cbLightPos.get() };
+	m_device.context()->PSSetConstantBuffers(0, 2, psb); //Pixel Shaders - 0: surfaceColor, 1: lightPos, 2: mapMtx
+}
+
+void RoomDemo::UpdateCameraCB(XMMATRIX viewMtx)
+{
+	XMVECTOR det;
+	XMMATRIX invViewMtx = XMMatrixInverse(&det, viewMtx);
+	XMFLOAT4X4 view[2];
+	XMStoreFloat4x4(view, viewMtx);
+	XMStoreFloat4x4(view + 1, invViewMtx);
+	UpdateBuffer(m_cbViewMtx, view);
+}
+
+
+void mini::gk2::RoomDemo::UpdateParticles(float dt)
+{
+	auto verts = m_particles.Update(dt, m_camera.getCameraPosition());
+	UpdateBuffer(m_vbParticles, verts);
+}
+
+void RoomDemo::Update(const Clock& c)
+{
+	double dt = c.getFrameTime();
+	HandleCameraInput(dt);
+	UpdateParticles(dt);
+}
+
+void RoomDemo::SetWorldMtx(DirectX::XMFLOAT4X4 mtx)
+{
+	UpdateBuffer(m_cbWorldMtx, mtx);
+}
+
+void mini::gk2::RoomDemo::SetShaders(const dx_ptr<ID3D11VertexShader>& vs, const dx_ptr<ID3D11PixelShader>& ps)
+{
+	m_device.context()->VSSetShader(vs.get(), nullptr, 0);
+	m_device.context()->PSSetShader(ps.get(), nullptr, 0);
+}
+
+void mini::gk2::RoomDemo::SetTextures(std::initializer_list<ID3D11ShaderResourceView*> resList, const dx_ptr<ID3D11SamplerState>& sampler)
+{
+	m_device.context()->PSSetShaderResources(0, resList.size(), resList.begin());
+	auto s_ptr = sampler.get();
+	m_device.context()->PSSetSamplers(0, 1, &s_ptr);
+}
+
+void RoomDemo::DrawMesh(const Mesh& m, DirectX::XMFLOAT4X4 worldMtx)
+{
+	SetWorldMtx(worldMtx);
+	m.Render(m_device.context());
+}
+
+void RoomDemo::DrawParticles()
+{
+	//Set input layout, primitive topology, shaders, vertex buffer, and draw particles
+	SetTextures({ m_smokeTexture.get(), m_opacityTexture.get() });
+	m_device.context()->IASetInputLayout(m_particleLayout.get());
+	SetShaders(m_particleVS, m_particlePS);
+	m_device.context()->GSSetShader(m_particleGS.get(), nullptr, 0);
+	m_device.context()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_POINTLIST);
+	unsigned int stride = sizeof(ParticleVertex);
+	unsigned int offset = 0;
+	auto vb = m_vbParticles.get();
+	m_device.context()->IASetVertexBuffers(0, 1, &vb, &stride, &offset);
+	m_device.context()->Draw(m_particles.particlesCount(), 0);
+
+	//Reset layout, primitive topology and geometry shader
+	m_device.context()->GSSetShader(nullptr, nullptr, 0);
+	m_device.context()->IASetInputLayout(m_inputlayout.get());
+	m_device.context()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+}
+
+void RoomDemo::DrawScene()
+{
+	UpdateBuffer(m_cbSurfaceColor, XMFLOAT4{ 1.0f, 0.7f, 0.7f, 1.0f });
+	for (auto& wallMtx : m_wallsMtx)
+		DrawMesh(m_wall, wallMtx);
+
+	UpdateBuffer(m_cbSurfaceColor, XMFLOAT4{ 0.7f, 0.2f, 0.7f, 1.0f });
+	DrawMesh(m_cylinder, m_cylinderMtx);
+	m_device.context()->RSSetState(nullptr);
+}
+
+void RoomDemo::Render()
+{
+	Base::Render();
+
+	// Set up view port of the appropriate size
+	UpdateBuffer(m_cbLightPos, m_lightPos);
+	UpdateBuffer(m_cbProjMtx, m_projMtx);
+	UpdateCameraCB();
+
+	// Bind m_lightMap and m_shadowMap textures then draw objects and particles using light&shadow pixel shader
+	SetShaders(m_phongVS, m_phongPS);
+
+	DrawScene();
+
+	m_device.context()->OMSetBlendState(m_bsAlpha.get(), nullptr, UINT_MAX);
+	m_device.context()->OMSetDepthStencilState(m_dssNoWrite.get(), 0);
+	DrawParticles();
+	m_device.context()->OMSetBlendState(nullptr, nullptr, UINT_MAX);
+	m_device.context()->OMSetDepthStencilState(nullptr, 0);
+}
---
+++ the-pink-puma/scene.h
@@ -0,0 +1,74 @@
+#pragma once
+#include "dxApplication.h"
+#include "mesh.h"
+#include "particleSystem.h"
+
+namespace mini::gk2
+{
+	class RoomDemo : public DxApplication
+	{
+	public:
+		using Base = DxApplication;
+
+		explicit RoomDemo(HINSTANCE appInstance);
+
+	protected:
+		void Update(const Clock& dt) override;
+		void Render() override;
+
+	private:
+#pragma region CONSTANTS
+		static constexpr float TABLE_H = 1.0f;
+		static constexpr float TABLE_TOP_H = 0.1f;
+		static constexpr float TABLE_R = 1.5f;
+		static constexpr unsigned int MAP_SIZE = 1024;
+		static constexpr float LIGHT_NEAR = 0.35f;
+		static constexpr float LIGHT_FAR = 5.5f;
+		static constexpr float LIGHT_FOV_ANGLE = DirectX::XM_PI / 3.0f;
+
+#pragma endregion
+		dx_ptr<ID3D11Buffer> m_cbWorldMtx, //vertex shader constant buffer slot 0
+			m_cbProjMtx;	//vertex shader constant buffer slot 2 & geometry shader constant buffer slot 0
+		dx_ptr<ID3D11Buffer> m_cbViewMtx; //vertex shader constant buffer slot 1
+		dx_ptr<ID3D11Buffer> m_cbSurfaceColor;	//pixel shader constant buffer slot 0
+		dx_ptr<ID3D11Buffer> m_cbLightPos; //pixel shader constant buffer slot 1
+
+		Mesh m_wall; //uses m_wallsMtx[6]
+		Mesh m_cylinder; //uses m_cylinderMtx
+		dx_ptr<ID3D11Buffer> m_vbParticles;
+
+		DirectX::XMFLOAT4X4 m_projMtx, m_wallsMtx[6], m_cylinderMtx, m_lightViewMtx[2], m_lightProjMtx;
+
+		DirectX::XMFLOAT4 m_lightPos;
+
+		dx_ptr<ID3D11SamplerState> m_sampler;
+
+		dx_ptr<ID3D11ShaderResourceView> m_smokeTexture, m_opacityTexture;
+
+		dx_ptr<ID3D11RasterizerState> m_rsCullNone;
+		dx_ptr<ID3D11BlendState> m_bsAlpha;
+		dx_ptr<ID3D11DepthStencilState> m_dssNoWrite;
+
+		dx_ptr<ID3D11InputLayout> m_inputlayout, m_particleLayout;
+
+		dx_ptr<ID3D11VertexShader> m_phongVS, m_particleVS;
+		dx_ptr<ID3D11GeometryShader> m_particleGS;
+		dx_ptr<ID3D11PixelShader> m_phongPS, m_particlePS;
+
+		ParticleSystem m_particles;
+
+		void UpdateCameraCB(DirectX::XMMATRIX viewMtx);
+		void UpdateCameraCB() { UpdateCameraCB(m_camera.getViewMatrix()); }
+		void UpdateParticles(float dt);
+
+		void DrawMesh(const Mesh& m, DirectX::XMFLOAT4X4 worldMtx);
+		void DrawParticles();
+
+		void SetWorldMtx(DirectX::XMFLOAT4X4 mtx);
+		void SetShaders(const dx_ptr<ID3D11VertexShader>& vs, const dx_ptr<ID3D11PixelShader>& ps);
+		void SetTextures(std::initializer_list<ID3D11ShaderResourceView*> resList, const dx_ptr<ID3D11SamplerState>& sampler);
+		void SetTextures(std::initializer_list<ID3D11ShaderResourceView*> resList) { SetTextures(std::move(resList), m_sampler); }
+
+		void DrawScene();
+	};
+}
